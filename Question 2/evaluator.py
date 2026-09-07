d7e9d30a917037868a9b7466d0aa72f3058aa985
Kevin Nguyen <nguyenngocchau@Nguyens-MacBook-Pro.local>
2026-09-08 01:07:03 +0930

    kevin

---
+++ Question 2/evaluator.py
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+def parse_level1_add_sub():
+    pass
+def parse_level2_mul_div():
+    pass
+def parse_level3_power():
+    pass
+def parse_level4_unary():
+    pass
+def parse_level5_primary():
+    pass
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def evaluate_file (input_path: str):
+    with open (input_path, "r") as f:
+        content = f.read()
+    for line in content:
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    evaluate_file ("input.txt")
