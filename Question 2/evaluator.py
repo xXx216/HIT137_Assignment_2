20b4bd61736a421e298a2f1f9e15f7604976c1fa
Kevin Nguyen <nguyenngocchau@Nguyens-MacBook-Pro.local>
2026-09-09 03:35:03 +0930

    =( this is so hard

--- Question 2/evaluator.py
+++ Question 2/evaluator.py
@@ -1,19 +1,23 @@
 
+# this is just an example: 2 + 3 * (4 + 1)
+numbers = ["2", "+", "3", "*", "(", "4", "+", "1", ")"] 
+
+position = 0
+
+
+def parse_level1_add_sub(): # 3 position left - middle - right 
+    pass
+def parse_level2_mul_div(): # 3 position left - middle - right 
+    pass
+def parse_level3_power(): # 3 position left - middle - right 
+    pass
+def parse_level4_unary(): # 3 position: middle - right
+    pass
 
 
 
-
-
-def parse_level1_add_sub():
-    pass
-def parse_level2_mul_div():
-    pass
-def parse_level3_power():
-    pass
-def parse_level4_unary():
-    pass
-def parse_level5_primary():
-    pass
+def parse_level5_primary(): # takes in brackets and numbers/ start from parse_lv5 backwards to 1 and everything comes back to lv5
+    left = parse_level4_unary()
     
 
 
@@ -38,6 +42,8 @@ def evaluate_file (input_path: str):
     with open (input_path, "r") as f:
         content = f.read()
     for line in content:
+        # something something
+        result = parse_level5_primary()
 
 
 
