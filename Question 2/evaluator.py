f1592f5bd3f3debeee8b02cc89841e8aabec7802
Kevin Nguyen <nguyenngocchau@Nguyens-MacBook-Pro.local>
2026-09-09 04:00:15 +0930

    update

--- Question 2/evaluator.py
+++ Question 2/evaluator.py
@@ -4,22 +4,34 @@ numbers = ["2", "+", "3", "*", "(", "4", "+", "1", ")"]
 
 position = 0
 
+#everytime a def parse takes a value, position is increased by 1.'
 
-def parse_level1_add_sub(): # 3 position left - middle - right 
-    pass
+
+
+def parse_level1_add_sub(): # 3 position left - middle - right/ start from lv 1 - 5, then return from 5 back to 1
+    left = parse_level2_mul_div()
 def parse_level2_mul_div(): # 3 position left - middle - right 
-    pass
+    left = parse_level3_power()
 def parse_level3_power(): # 3 position left - middle - right 
-    pass
-def parse_level4_unary(): # 3 position: middle - right
-    pass
-
-
-
-def parse_level5_primary(): # takes in brackets and numbers/ start from parse_lv5 backwards to 1 and everything comes back to lv5
     left = parse_level4_unary()
-    
+    if numbers[position] == "^":
+        ###
+def parse_level4_unary(): # 3 position: middle - right
+    if numbers[position] == "-":
+        ####
+    else:
+        return parse_level5_primary()
 
+
+
+def parse_level5_primary(): # takes in brackets and numbers
+    value = numbers[position]
+    if value.isdigit():
+        position += 1 
+        return value
+    
+    if value == "()"
+        # something 
 
 
 
@@ -43,7 +55,7 @@ def evaluate_file (input_path: str):
         content = f.read()
     for line in content:
         # something something
-        result = parse_level5_primary()
+        result = parse_level1_add_sub()
 
 
 
